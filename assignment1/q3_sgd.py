487de79089841c98466dd42302e2f27ece43ce5e
peter6888 <qq1800@gmail.com>
2018-01-19 20:15:08 -0800

    PSET1 q3 (f)

--- assignment1/q3_sgd.py
+++ assignment1/q3_sgd.py
@@ -85,7 +85,9 @@ def sgd(f, x0, step, iterations, postprocessing=None, useSaved=False,
 
         cost = None
         ### YOUR CODE HERE
-        raise NotImplementedError
+        cost, gradient = f(x)
+        x -= step*gradient
+        x = postprocessing(x)
         ### END YOUR CODE
 
         if iter % PRINT_EVERY == 0:
@@ -132,7 +134,10 @@ def your_sanity_checks():
     """
     print "Running your sanity checks..."
     ### YOUR CODE HERE
-    raise NotImplementedError
+    quad = lambda x: (np.sum(x ** 2 - 4), x * 2)
+    t3 = sgd(quad, 5, 0.1, 1000, PRINT_EVERY=100)
+    print "test 3 result:", t3
+    assert abs(t3) <= 1e-6
     ### END YOUR CODE
 
 
