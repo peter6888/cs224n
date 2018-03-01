2584868fabe13fc7edd42c82ba16413a9de08129
peter6888 <qq1800@gmail.com>
2018-02-28 20:30:30 -0800

    python3 version of pointer generator

---
+++ custom_project/pointer-generator/run_cmd.sh
@@ -0,0 +1,1 @@
+python run_summarization.py --mode=train --data_path=../../../nlp_project/data/finished_files/chunked/train_*.bin --vocab_path=../../../nlp_project/data/finished_files/vocab --log_root=/Users/peli/log --exp_name=baseline
