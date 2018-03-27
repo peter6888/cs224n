3208ff57c4bdb8b7074c78ec13cb42094040435d
peter6888 <qq1800@gmail.com>
2018-03-26 23:13:42 -0700

    Toy model of vector compare
    Toy model of vector compare. baseline done with 86.5% accuracy.

--- custom_project/group_vector.py
+++ custom_project/group_vector.py
@@ -14,6 +14,10 @@ Task, given two vectors, your model could tell if they are in same group
 
 import tensorflow as tf
 import numpy as np
+
+lr = 0.003
+vector_size = 3
+
 def generate_data(counts, size=3):
     ret = []
 
@@ -57,8 +61,9 @@ def pred_op(v_z):
 
     Returns: Distribution possibility [batch_size, 2], a binary classification
     '''
-    dense_layer = tf.layers.dense(inputs=v_z, units=2, kernel_initializer=tf.contrib.layers.xavier_initializer())
-    return tf.nn.softmax(dense_layer)
+    hidden_layer =  tf.layers.dense(inputs=v_z, units=vector_size, activation=tf.nn.relu, kernel_initializer=tf.contrib.layers.xavier_initializer())
+    pred = tf.layers.dense(inputs=hidden_layer, units=2, activation=tf.nn.relu, kernel_initializer=tf.contrib.layers.xavier_initializer())
+    return pred
 
 def loss_op(pred, y):
     '''
@@ -67,10 +72,14 @@ def loss_op(pred, y):
         y: [batch_size, 2], one-hot vector for true label, [1,0] - false, [0,1] - true
     Returns: loss
     '''
-    loss = tf.losses.sigmoid_cross_entropy(y, pred)
-    if y is not None: # Train
-        return loss
+    y_onehot = tf.one_hot(y, depth=2)
+    loss = tf.losses.softmax_cross_entropy(y_onehot, pred)
     return loss
+
+def train_op(loss):
+    optimizer = tf.train.AdamOptimizer(learning_rate=lr)
+    _train_op = optimizer.minimize(loss)
+    return _train_op
 
 def divide_data(inputs):
     '''
@@ -90,16 +99,50 @@ def divide_data(inputs):
     return ret
 
 if __name__ == "__main__":
-    l = generate_data(100)
+    l = generate_data(5000)
     dict_l = divide_data(l)
     print(len(dict_l["train"]),len(dict_l["val"]), len(dict_l["test"]))
-    a, b, c = zip(*dict_l["test"])
-    vector_size = 3
+
     tensor_a = tf.placeholder(shape=[None, vector_size], dtype=tf.float32)
     tensor_b = tf.placeholder(shape=[None, vector_size], dtype=tf.float32)
+    tensor_c = tf.placeholder(shape=[None], dtype=tf.int32)
     pred = pred_op(vector_compare_concat(tensor_a, tensor_b))
+    loss = loss_op(pred, tensor_c)
+    train = train_op(loss)
+    train_data = dict_l["train"]
+    val_data = dict_l["val"]
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-        _pred = sess.run(pred, feed_dict={tensor_a: a, tensor_b: b})
-        print(_pred.shape) # output shape [batch_size, 2] # binary classification
-        print(_pred)
+        for i in range(10000):
+            np.random.shuffle(train_data)
+            a, b, c = zip(* train_data)
+            _pred, _loss, _ = sess.run([pred, loss, train], feed_dict={tensor_a: a, tensor_b: b, tensor_c: c})
+            #print(_pred.shape) # output shape [batch_size, 2] # binary classification
+            #print(_pred)
+            if i%1000 == 0:
+                print("Cross-Entropy loss:{}".format(_loss))
+                if i > 5000:
+                    np.random.shuffle(val_data)
+                    val_a, val_b, val_y = zip(*val_data)
+                    _c = sess.run(tf.nn.softmax(pred), feed_dict={tensor_a: val_a, tensor_b: val_b})
+                    y_hat = np.argmax(_c, axis=1)
+                    print(np.sum(y_hat==np.array(val_y)))
+                    #print(y_hat.shape)
+                    #print(len(val_y))
+'''
+Cross-Entropy loss:441.1654968261719
+Cross-Entropy loss:0.30894121527671814
+Cross-Entropy loss:0.24041341245174408
+Cross-Entropy loss:0.21878936886787415
+Cross-Entropy loss:0.20873117446899414
+Cross-Entropy loss:0.20419609546661377
+Cross-Entropy loss:0.20192311704158783
+173
+Cross-Entropy loss:0.20131425559520721
+173
+Cross-Entropy loss:0.20093075931072235
+173
+Cross-Entropy loss:0.20085400342941284
+173
+'''
+
